ec0508916130c7508c77be9eebe1c88fe15c79f4
athul16 <64622808+athul16@users.noreply.github.com>
2024-04-25 02:44:14 -0400

    Update and rename bulletinBoard.js to data.js

---
+++ data.js
@@ -0,0 +1,41 @@
+var threads= [
+    {
+        id: 1,
+        title: "Thread 1",
+        author: "Aaron",
+        date: Date.now(),
+        content: "Thread content",
+        comments: [
+            { 
+                author: "Jack",
+                date: Date.now(),
+                content: "Hey there"
+            },
+            {
+                author: "Arthur",
+                date: Date. now(),
+                content: "Hey to you too"
+            }
+        ]
+    },
+    {
+        id: 2,
+        title: "Thread 2",
+        author: "Aaron",
+        date: Date.now(),
+        content: "Thread content",
+        comments: [
+            { 
+                author: "Jack",
+                date: Date.now(),
+                content: "Hey there"
+            },
+            {
+                author: "Arthur",
+                date: Date. now(),
+                content: "Hey to you too"
+            }
+        ]
+    }
+
+]
