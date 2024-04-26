fb928f969af32950b3db3583afb10fdc0934da83
ethanw2457 <130408385+ethanw2457@users.noreply.github.com>
2024-04-26 00:22:19 +0000

    profiles

---
+++ clubProfile.js
@@ -0,0 +1,8 @@
+var typed = new Typed("#typed", {
+  strings: ["Welcome to my personal Website"],
+  typeSpeed: 110,
+  startDelay: 500,
+  loop: true,
+  loopCount: Infinity,
+  backDelay: 1000
+});
