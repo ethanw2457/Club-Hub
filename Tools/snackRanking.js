e9ea28d376ed602bf54bb7a4c65d80e4c1880791
athul16 <64622808+athul16@users.noreply.github.com>
2024-04-27 15:18:21 -0400

    Update snackRanking.js

--- Tools/snackRanking.js
+++ Tools/snackRanking.js
@@ -1,3 +1,23 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+
+// TODO: Replace the following with your app's Firebase project configuration
+// See: https://firebase.google.com/docs/web/learn-more#config-object
+const firebaseConfig = {
+  // ...
+  // The value of `databaseURL` depends on the location of the database
+  databaseURL: "https://club-central-2af6e-default-rtdb.firebaseio.com/",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Realtime Database and get a reference to the service
+const db = getDatabase(app);
+
+
+
+
 // Handle form submission
 const rentalForm = document.getElementById('rental-form');
 const submissionResult = document.querySelector('.submission-result');
@@ -16,6 +36,21 @@ rentalForm.addEventListener('submit', function(e) {
   submissionResult.classList.add('show');
   rentalForm.reset();
 });
+
+//save to database
+
+function save() {
+    db.ref('snacks/' + name).set({
+       name: name,
+       email: email,
+       destination: destination,
+       bike: bike, 
+       price: price,
+
+    })
+
+    alert('Saved')
+}
 
 // Handle bike details toggle
 const bikeDetailsToggle = document.querySelectorAll('.bike-details-toggle');
