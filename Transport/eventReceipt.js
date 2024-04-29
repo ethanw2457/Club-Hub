a940d02eb3156de6be337d105461ee92ad3caf16
ethanw2457 <130408385+ethanw2457@users.noreply.github.com>
2024-04-29 19:17:43 +0000

    s

--- Transport/eventReceipt.js
+++ Transport/eventReceipt.js
@@ -1,3 +1,42 @@
+import {initializeApp} from "https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js";
+import {getDatabase, ref, set, child, get, remove, update} from "https://www.gstatic.com/firebasejs/10.11.1/firebase-database.js";
+import {getStorage, ref as sref, getDownloadURL, uploadBytes} from "https://www.gstatic.com/firebasejs/10.11.1/firebase-storage.js";
+// Header Package=============================================================================================================
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDjMBUC1EhrOSzzgId-sglmdmJJ4kCyV5Q",
+  authDomain: "club-central-2af6e.firebaseapp.com",
+  databaseURL: "https://club-central-2af6e-default-rtdb.firebaseio.com",
+  projectId: "club-central-2af6e",
+  storageBucket: "club-central-2af6e.appspot.com",
+  messagingSenderId: "578174084496",
+  appId: "1:578174084496:web:13f92682f267332f62ff15",
+  measurementId: "G-FKR7SRZ915"
+};
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+const storage = getStorage(app);
+
+
+document.getElementById("sign-out").addEventListener('click', signOut);
+
+function signOut() {
+  sessionStorage.setItem("currentUser", "");
+}
+getDownloadURL(sref(storage, 'users/' + sessionStorage.getItem("currentUser")))
+.then((url) => {
+
+  // Or inserted into an <img> element
+  const img = document.getElementById('profile-pic');
+  img.setAttribute('src', url);
+});
+get(child(ref(db), 'users/' + sessionStorage.getItem("currentUser"))).then((snapshot) => {
+  if (!snapshot.val().creator)
+    document.getElementById("createClub").style.display = "none";
+
+});
+// End of Header Package================================================================================================
 
 document.getElementById("eventname").innerHTML = localStorage.getItem("event");
 document.getElementById("eventdetails").innerHTML = "Location: " + localStorage.getItem("eventaddress") + "<br>Date: " + localStorage.getItem("eventdate");
@@ -10,7 +49,7 @@ document.getElementById("carpoolerinfo").innerHTML = "Carpooler" + "<br>" + loca
 document.getElementById("carpooler").innerHTML = localStorage.getItem("user" + cpnum);
 
 
-function initMap() {
+export function initMap() {
   const directionsService = new google.maps.DirectionsService();
   const directionsRenderer = new google.maps.DirectionsRenderer();
   const map = new google.maps.Map(document.getElementById("map"), {
@@ -44,7 +83,7 @@ function calculateAndDisplayRoute(directionsService, directionsRenderer) {
   })
   .catch((e) => window.alert("Directions request failed due to " + status));
 }
-
+initMap();
 //window.initMap = initMap;
 
 
